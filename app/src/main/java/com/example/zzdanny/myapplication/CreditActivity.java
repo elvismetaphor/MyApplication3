60e67205235473c79a272a41bd02488c21af3288
Elvis Lin <elvislin@Elviss-MacBook-Pro.local>
2016-11-20 07:40:03 +0800

    Show overspend notify settings

--- app/src/main/java/com/example/zzdanny/myapplication/CreditActivity.java
+++ app/src/main/java/com/example/zzdanny/myapplication/CreditActivity.java
@@ -3,6 +3,7 @@ package com.example.zzdanny.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -54,6 +55,16 @@ public class CreditActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(CreditActivity.this,ExrateActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(CreditActivity.this, OverspendNotifySettings.class);
                 startActivity(intent);
                 finish();
             }
